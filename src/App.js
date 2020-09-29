1b5bc05aaf51d36adee8be08a00c3cc9bf35219d
fabcodingzest <fabrizvi786@gmail.com>
2020-09-29 23:49:01 +0530

    made changes for responsiveness

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="max-w-4xl mx-auto min-h-screen h-screen w-screen bg-black">
       <h1 className="text-2xl text-white text-center">Markdown Live Previewer</h1>
-      <div className="bg-white p-4 flex flex-col min-h-3/4 md:flex-row justify-center items-start rounded">
+      <div className="bg-white p-4 flex flex-col min-h-2/4 md:flex-row justify-center items-start rounded mx-2">
       <Editor onChange={handleOnChange} markdown={md} />
       <Previewer markdown={md} />
       </div>
